94cde88ae9b748294c6313893eb4eaac27024d9b
jjoylee <ekdnlt1995@gmail.com>
2020-02-12 00:29:20 +0900

    blog first commit test

---
+++ gatsby-meta-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  title: `jjoy`,
+  description: ``,
+  author: `[JeongHeeLee]`,
+  introduction: ``,
+  siteUrl: `https://jjoy.netlify.com`, // Your blog site url
+  social: {
+    twitter: ``, // Your Twitter account
+    github: `jjoylee`, // Your GitHub account
+    medium: ``, // Your Medium account
+    facebook: ``, // Your Facebook account
+  },
+  icon: `content/assets/felog.png`, // Add your favicon
+  keywords: [`blog`],
+  comment: {
+    disqusShortName: '', // Your disqus-short-name. check disqus.com.
+    utterances: 'jjoylee/jjoy', // Your repository for archive comment
+  },
+  configs: {
+    countOfInitialPost: 10, // Config your initial count of post
+  },
+  sponsor: {
+    buyMeACoffeeId: 'jjoylee',
+  },
+  share: {
+    facebookAppId: '', // Add facebookAppId for using facebook share feature v3.2
+  },
+  ga: '', // Add your google analytics tranking ID
+}
